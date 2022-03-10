72df3c268158cad04e07a9a10ece5f864da4f376
Mohammad YousefiPour <moyousefipour79@gmail.com>
2022-03-11 01:29:21 +0330

    make test case
    making amount of empty test case files.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Quera_Test_Case_Maker
 Making test case for your programming questions that want to use in Quera.
+
+## Make
+The make.py is use for making test case for who want making question.
+Run the program and enter a number for making how many test case that you want.
--- make.py
+++ make.py
@@ -1,5 +1,6 @@
 import shutil
 import os
+import glob
 
 current_dir = os.getcwd()
 test_path = os.path.join(current_dir, 'Test')
@@ -31,13 +32,88 @@ def makeoutput(num):
     f.close()
 
 
+# def makeinput(num, *args):
+#     '''
+#     Make intput files for testcases. `num` is use for number of files.
+#     '''
+#     full_path = os.path.join(current_dir, 'in')
+#     os.mkdir(full_path)
+#     os.chdir('in')
+#     print()
+#     with open('input%d.txt' % (num), 'w') as f:
+#         for items in args:
+#             if isinstance(items, list):
+#                 f.write('\n')
+#                 for item in items:
+#                     f.write(item)
+#             else:
+#                 f.write(items)
+#     f.close()
+
+
+# def makeoutput(num, answer):
+#     '''
+#     Make output files for testcases. `num` is use for number of files.
+#     '''
+#     full_path = os.path.join(current_dir, 'out')
+#     os.mkdir(full_path)
+#     os.chdir(full_path)
+#     with open('output%d.txt' % (num), 'w') as f:
+#         f.write(answer)
+#     f.close()
+
+# def simplify(args: list):
+#     '''
+#     simplify the input for passing to file
+#     '''
+#     for i in range(len(args)):
+#         if isinstance(args[i], list):
+#             args.insert(i, '\n')
+#             args[i] = ' '.join(args[i])
+#     return ' '.join(args)
+
+# def execute_file(file, args: list):
+#     '''
+#     Execute your answer/code and test with testcases.
+#     '''
+#     args = simplify(args)
+#     index = file.rfind('.')
+#     ext = file[index:]
+#     if ext == '.py':
+#         command_list = ['python', file]
+#         complete = subprocess.run(
+#             command_list, input=args, capture_output=True)
+#         return complete.stdout.decode()
+
+
+def get_input():
+    '''
+    Get input of program for save in input file and execute for output.
+    '''
+    input_list = []
+    while True:
+        print('Pass your inputs:')
+        input_list.append(input())
+        print('Any other input? [y/n]')
+        if input() == 'n':
+            break
+    return input_list
+
+
 def main():
     '''
     This is the main of our program
     '''
-    num = int(input('How many test case do you need? \n'))
+    # print('\n---------Files---------\n')
+    # files_list = [i for i in glob.glob(r'*.py') if i != 'make.py']
+    # print(files_list)
+    # print('\n---------Code---------\n')
+    # file = input('Choose your code/answer? \n')
+    print('\n---------Test---------\n')
+    num = int(input('How many testcases do you need? \n'))
     try:
         for i in range(1, num+1):
+            # input_list = get_input()
             makeinput(i)
             makeoutput(i)
     except FileExistsError:
