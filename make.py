2d9174a83cd2c65d82cb7a38f1f8bdd183770343
Mohammad YousefiPour <moyousefipour79@gmail.com>
2022-11-20 00:27:40 +0330

    Update make.py

--- make.py
+++ make.py
@@ -1,53 +1,66 @@
 import shutil
 import os
 
-current_dir = os.getcwd()
-test_path = os.path.join(current_dir, 'Test')
 
 
-def makeinput(num):
-    '''
-    Make inpput testcase file
-    '''
-    full_path = os.path.join(test_path, 'in')
-    if not os.path.exists(full_path):
-        os.makedirs(full_path)
-    os.chdir(full_path)
-    with open('input%d.txt' % (num), 'w') as f:
-        pass
-    f.close()
+class TestMaker:
+    current_dir = os.getcwd()
+    test_path = os.path.join(current_dir, 'Test')
+    
+    def exist_folder(self):
+        '''
+        Check Test folder is Exist.
+        '''
+        if os.path.exists(TestMaker.test_path):
+            raise FileExistsError
 
+    def makeinput(self,num):
+        '''
+        Make inpput testcase file
+        '''
+        full_path = os.path.join(TestMaker.test_path, 'in')
+        if not os.path.exists(full_path):
+            os.makedirs(full_path)
+        os.chdir(full_path)
+        with open('input%d.txt' % (num), 'w') as f:
+            pass
+        f.close()
 
-def makeoutput(num):
-    '''
-    Make output testcase file
-    '''
-    full_path = os.path.join(test_path, 'out')
-    if not os.path.exists(full_path):
-        os.makedirs(full_path)
-    os.chdir(full_path)
-    with open('output%d.txt' % (num), 'w') as f:
-        pass
-    f.close()
-
+    def makeoutput(self,num):
+        '''
+        Make output testcase file
+        '''
+        full_path = os.path.join(TestMaker.test_path, 'out')
+        if not os.path.exists(full_path):
+            os.makedirs(full_path)
+        os.chdir(full_path)
+        with open('output%d.txt' % (num), 'w') as f:
+            pass
+        f.close()
+    
 
 def main():
     '''
     This is the main of our program
     '''
-    num = int(input('How many test case do you need? \n'))
+    test=TestMaker()
     try:
+        test.exist_folder()
+        num = int(input('How many test case do you need? \n'))
         for i in range(1, num+1):
-            makeinput(i)
-            makeoutput(i)
+            test.makeinput(i)
+            test.makeoutput(i)
+        print('Done')
+        command=input('Enter to Continue :)')
+        if command=='\n':
+            exit()
     except FileExistsError:
-        print('File Exist, Do you wanna delete it? [y/n]')
+        print('File or Directory Exist, Do you wanna delete it? [y/n]')
         if input() == 'y':
             shutil.rmtree('Test')
             main()
         else:
             exit()
-
 
 if __name__ == '__main__':
     main()
